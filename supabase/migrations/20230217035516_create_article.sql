5ae51c96a9be8fd2f7fd84f9045c93250bbb9087
Sam Evans <evans.sam.t@gmail.com>
2023-02-16 21:29:16 -0700

    Init supabase local database

---
+++ supabase/migrations/20230217035516_create_article.sql
@@ -0,0 +1,23 @@
+create table "public"."articles" (
+    "id" uuid not null default uuid_generate_v4(),
+    "created_at" timestamp with time zone default now(),
+    "slug" text not null,
+    "title" text not null,
+    "image" text,
+    "content" text,
+    "excerpt" text,
+    "author" text
+);
+
+
+alter table "public"."articles" enable row level security;
+
+CREATE UNIQUE INDEX articles_pkey ON public.articles USING btree (id);
+
+CREATE UNIQUE INDEX articles_slug_key ON public.articles USING btree (slug);
+
+alter table "public"."articles" add constraint "articles_pkey" PRIMARY KEY using index "articles_pkey";
+
+alter table "public"."articles" add constraint "articles_slug_key" UNIQUE using index "articles_slug_key";
+
+
