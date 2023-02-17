74b22b8e0d7018cbde531f29b24a44422d85ee97
Sam Evans <evans.sam.t@gmail.com>
2023-02-17 10:45:43 -0700

    Migrates to supabase js client
    - Moves source files to src in all apps
    - Adds some typescript voodoo to get types working

--- apps/news-site/src/components/meta.tsx
+++ apps/news-site/src/components/meta.tsx
@@ -31,7 +31,7 @@ const Meta = () => {
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
       <meta name="theme-color" content="#000" />
-      <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+      {/*<link rel="alternate" type="application/rss+xml" href="/feed.xml" />*/}
       <meta
         name="description"
         content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
