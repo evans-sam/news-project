d9e3fbb13266370eddad03952e1b0e53265c4f3c
Sam Evans <98488535+evans-sam@users.noreply.github.com>
2023-02-17 20:42:33 -0700

    Adds news client (#1)
    * Adds news client
    
    * Updates lock file

---
+++ packages/clients/src/news-api-client/types.ts
@@ -0,0 +1,139 @@
+export type NewsRequest = {
+  q: string;
+  pageSize: number;
+  page: number;
+};
+
+export type NewsResponse<K> = K | ErrorResponse;
+
+export type EverythingRequest = NewsRequest & {
+  searchIn?: SearchIn[];
+  sources?: string[];
+  domains?: string[];
+  excludeDomains?: string[];
+  from?: Date;
+  to?: Date;
+  language?: Language;
+  sortBy?: SortBy;
+};
+
+export type HeadlinesRequest = NewsRequest & {
+  category?: Category;
+} & ({ country: Country } | { sources: string[] } | {});
+
+export type SourcesRequest = {
+  category?: Category;
+  language?: Language;
+  country?: Country;
+};
+
+export type ArticleResponse = {
+  status: 'ok';
+  totalResults: number;
+  articles: Article[];
+};
+
+export type SourcesResponse = {
+  status: 'ok';
+  sources: Source[];
+};
+
+export type ErrorResponse = {
+  status: 'error';
+  code: ErrorCode;
+  message: string;
+};
+
+export type Article = {
+  source: Source;
+  author: string;
+  title: string;
+  description: string;
+  url: string;
+  urlToImage: string;
+  publishedAt?: Date;
+  content: string;
+};
+
+export type Category = 'business' | 'entertainment' | 'general' | 'health' | 'science' | 'sports' | 'technology';
+export type Country =
+  | 'ae'
+  | 'ar'
+  | 'at'
+  | 'au'
+  | 'be'
+  | 'bg'
+  | 'br'
+  | 'ca'
+  | 'ch'
+  | 'cn'
+  | 'co'
+  | 'cu'
+  | 'cz'
+  | 'de'
+  | 'eg'
+  | 'fr'
+  | 'gb'
+  | 'gr'
+  | 'hk'
+  | 'hu'
+  | 'id'
+  | 'ie'
+  | 'il'
+  | 'in'
+  | 'it'
+  | 'jp'
+  | 'kr'
+  | 'lt'
+  | 'lv'
+  | 'ma'
+  | 'mx'
+  | 'my'
+  | 'ng'
+  | 'nl'
+  | 'no'
+  | 'nz'
+  | 'ph'
+  | 'pl'
+  | 'pt'
+  | 'ro'
+  | 'rs'
+  | 'ru'
+  | 'sa'
+  | 'se'
+  | 'sg'
+  | 'si'
+  | 'sk'
+  | 'th'
+  | 'tr'
+  | 'tw'
+  | 'ua'
+  | 'us'
+  | 've'
+  | 'za';
+export type ErrorCode =
+  | 'apiKeyDisabled'
+  | 'apiKeyExhausted'
+  | 'apiKeyInvalid'
+  | 'parameterInvalid'
+  | 'parameterMissing'
+  | 'rateLimited'
+  | 'sourcesTooMany'
+  | 'sourceDoesNotExist'
+  | 'unexpectedError';
+export type SearchIn = 'title' | 'content' | 'description';
+export type Language = 'ar' | 'de' | 'en' | 'es' | 'fr' | 'he' | 'it' | 'nl' | 'no' | 'pt' | 'ru' | 'se' | 'ud' | 'zh';
+export type SortBy = 'relevancy' | 'popularity' | 'publishedAt';
+export type Source = {
+  id: string;
+  name: string;
+} & (
+  | {
+      description: string;
+      url: string;
+      category: Category;
+      language: Language;
+      country: Country;
+    }
+  | {}
+);
